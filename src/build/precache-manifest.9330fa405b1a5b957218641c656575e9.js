7f28f1cb59622b95a9b1e294997c728f7369ce39
Andrew Dunn <atd2005@gmail.com>
2019-08-07 15:34:47 -0400

    First commit

---
+++ src/build/precache-manifest.9330fa405b1a5b957218641c656575e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6511a88b61c659c88df8029704d57c23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15fb5c41b57d1ef535e6",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "bb660b66d97ca872da68",
+    "url": "/static/js/2.ad55af6c.chunk.js"
+  },
+  {
+    "revision": "15fb5c41b57d1ef535e6",
+    "url": "/static/js/main.70d4c092.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
